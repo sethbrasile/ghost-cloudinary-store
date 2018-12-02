10e34cf1b64fbc415ec976cae2adb77d21d62c78
Marco Mornati <139323+mmornati@users.noreply.github.com>
2018-12-02 19:22:30 +0100

    Fixed failing upload tet (#24)

--- test/test.js
+++ test/test.js
@@ -53,6 +53,11 @@ describe('Image Upload', function () {
     };
 
     before(function () {
+        cloudinary.config({ 
+            cloud_name: 'blog-mornati-net', 
+            api_key: '874837483274837', 
+            api_secret: 'a676b67565c6767a6767d6767f676fe1' 
+          }); 
         cloudinaryAdapter = new CloudinaryAdapter(baseConfig);
     });
 
